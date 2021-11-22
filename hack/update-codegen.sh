63268afe91c3681f2e6f345e1e0f2e972ab11123
chenxing <chenxing@bookan.com.cn>
2021-11-22 15:04:15 +0800

    add hack dir, use code-generator to gen code

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -6,5 +6,5 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/xing393939/samplecrd-code/pkg/client github.com/xing393939/samplecrd-code/pkg/apis \
   samplecrd:v1 \
-  --output-base "${SCRIPT_ROOT}/../.." \
+  --output-base "${SCRIPT_ROOT}/../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
